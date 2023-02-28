0640481604909b0062f9c72d520b2357e8641e83
George Metsis <gmetsis@optechain.com>
2023-02-28 19:25:22 +0100

    implement CRUD operations

--- src/main/java/gr/kariera/mindthecode/MyFirstProject/PersonController.java
+++ src/main/java/gr/kariera/mindthecode/MyFirstProject/PersonController.java
@@ -1,6 +1,10 @@
 package gr.kariera.mindthecode.MyFirstProject;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.List;
 
 @RestController
 public class PersonController {
@@ -9,6 +13,14 @@ public class PersonController {
 
     public PersonController(PersonRepository repo) {
         this.repo = repo;
+    }
+
+    @PutMapping("/persons/{id}")
+    public Person update(@PathVariable Integer id, @RequestBody Person person) {
+        if (!id.equals(person.getId())) {
+            throw new HttpClientErrorException(HttpStatusCode.valueOf(400), "id in path does not patch id in body");
+        }
+        return repo.save(person);
     }
 
     @PostMapping("/persons")
@@ -20,5 +32,17 @@ public class PersonController {
     public Person one(@PathVariable Integer id) {
         return repo.findById(id)
                 .orElseThrow();
+    }
+
+    @GetMapping("/persons")
+    public List<Person> all() {
+        return repo.findAll();
+    }
+
+    @DeleteMapping("/persons/{id}")
+    public void delete(@PathVariable Integer id) {
+        Person match = repo.findById(id)
+                .orElseThrow();
+        repo.delete(match);
     }
 }
