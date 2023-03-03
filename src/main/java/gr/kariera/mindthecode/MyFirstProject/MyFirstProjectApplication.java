5e4dc0e69b99fe17dab56a84473cbf1492106ce2
George Metsis <gmetsis@optechain.com>
2023-03-03 19:56:36 +0200

    make app compatible with JBOSS(wildfly)

--- src/main/java/gr/kariera/mindthecode/MyFirstProject/MyFirstProjectApplication.java
+++ src/main/java/gr/kariera/mindthecode/MyFirstProject/MyFirstProjectApplication.java
@@ -2,10 +2,19 @@ package gr.kariera.mindthecode.MyFirstProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyFirstProjectApplication {
+public class MyFirstProjectApplication  extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstProjectApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<MyFirstProjectApplication> applicationClass = MyFirstProjectApplication.class;
 }
